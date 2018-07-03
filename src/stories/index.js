66ccf3755f522ee3fef781fcd99a65def013e2a5
ia3andy <adamevin@redhat.com>
2018-07-03 17:20:24 +0200

    add chatbot story

--- src/stories/index.js
+++ src/stories/index.js
@@ -2,18 +2,17 @@ import React from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
-import { linkTo } from '@storybook/addon-links';
 
-import { Button, Welcome } from '@storybook/react/demo';
+import ChatBot from 'react-simple-chatbot';
 
-storiesOf('Welcome', module).add('to Storybook', () => <Welcome showApp={linkTo('Button')} />);
-
-storiesOf('Button', module)
-  .add('with text', () => <Button onClick={action('clicked')}>Hello Button</Button>)
-  .add('with some emoji', () => (
-    <Button onClick={action('clicked')}>
-      <span role="img" aria-label="so cool">
-        😀 😎 👍 💯
-      </span>
-    </Button>
-  ));
+storiesOf('Chat-Bot', module)
+  .add('Hello World', () => (<ChatBot
+    headerTitle="Cloud App Creator"
+    steps={[
+      {
+        id: 'hello-world',
+        message: 'Hello World!',
+        end: true,
+      },
+    ]}
+  />));
