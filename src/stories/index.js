9a3e32f4e2ed6df95605aff7b5c4c493db079e9e
ia3andy <adamevin@redhat.com>
2018-07-03 17:31:09 +0200

    hello you

--- src/stories/index.js
+++ src/stories/index.js
@@ -6,13 +6,24 @@ import { action } from '@storybook/addon-actions';
 import ChatBot from 'react-simple-chatbot';
 
 storiesOf('Chat-Bot', module)
-  .add('Hello World', () => (<ChatBot
+  .add('Hello you', () => (<ChatBot
     headerTitle="Cloud App Creator"
     steps={[
       {
-        id: 'hello-world',
-        message: 'Hello World!',
+        id: '1',
+        message: 'What is your name?',
+        trigger: '2',
+      },
+      {
+        id: '2',
+        user: true,
+        trigger: '3',
+      },
+      {
+        id: '3',
+        message: 'Hi {previousValue}, nice to meet you!',
         end: true,
       },
     ]}
+
   />));
