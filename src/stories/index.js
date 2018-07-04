ff3ead31f3fd5343de34a99a49b28d6510c1ca80
ia3andy <adamevin@redhat.com>
2018-07-04 16:52:19 +0200

    demo alr

--- src/stories/index.js
+++ src/stories/index.js
@@ -32,28 +32,34 @@ storiesOf('Chat-Bot', module).add('Hello you', () => {
           id: '3',
           message:
             "OK, we'll set you up with something called a message broker, and this is provided by Red Hat AMQ. Would you like new messages sent here to be handled by one listener (Queue) or broadcast to many (Topic)",
-          trigger: '4',
+          trigger: '4'
         },
         {
           id: '4',
           options: [
             { value: 1, label: 'Queue', trigger: '5' },
-            { value: 2, label: 'Topic', trigger: '5' }
+            { value: 2, label: 'Topic', trigger: '6' }
           ]
         },
         {
           id: '5',
           message:
-            'No problem; what name would you like to give this Topic?  An example might look like myApplication/greetings',
-          trigger: triggerInput('newsserver/newsalert', 6)
+            "Sorry I only support Topic for now... I'll assume you want a Topic then.",
+          trigger: '6'
         },
         {
           id: '6',
-          user: true,
-          trigger: '7'
+          message:
+            'No problem; what name would you like to give this Topic?  An example might look like myApplication/greetings',
+          trigger: triggerInput('newsserver/newsalert', 7)
         },
         {
           id: '7',
+          user: true,
+          trigger: '8'
+        },
+        {
+          id: '8',
           message:
             "Great; we'll set that up for you.  Would you like to set up message listeners now?  These are the application pieces which receive a message and may take some action.",
           end: true
